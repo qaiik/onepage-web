be3e0b1a82499e180f7aa218861bf60fcc0f652c
qaiik <82181231+qaiik@users.noreply.github.com>
2021-11-23 11:20:51 -0700

    Create onepage.js

---
+++ src/onepage.js
@@ -0,0 +1,27 @@
+o = {
+  router: class {
+    constructor() {
+      this.routemklinks = {}
+    }
+    AddRoute(name, html) {
+      this.routemklinks["#" + name] = html;
+    }
+    On404(html) {
+      this.routemklinks["404"] = html;
+    start() {
+      window.addEventListener('locationchange', function(){
+        if (typeof this.routemklinks[location.hash] === "string") {
+          document.querySelector("#op-view").innerHTML = this.routemklinks[location.hash];
+        } else {
+          if (typeof this.routemklinks["404"] === "string") {
+            document.querySelector("#op-view").innerHTML = this.routemklinks["404"]
+          } else {
+            document.querySelector("#op-view").innerHTML = "404; That page was not found."
+          }
+        }
+      })
+    }
+  }
+}
+    
+      
